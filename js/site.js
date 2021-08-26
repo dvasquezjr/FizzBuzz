bde5f41d7ad57e8819eb1f5f471dc9179db01a48
dvasquezjr <77078244+dvasquezjr@users.noreply.github.com>
2021-08-26 11:37:17 -0500

    Added Application

--- js/site.js
+++ js/site.js
@@ -1,4 +1,100 @@
-//Call Hello World
-function HelloWorld(){
-  alert("Hello World");
+//Get values from the input
+//Controller function
+function getValues (){
+
+  //Get Buzz Value and Fizz Value
+  let fizzValue = document.getElementById("fizzValue").value;
+  let buzzValue = document.getElementById("buzzValue").value;
+
+  //Parse for numbers
+  fizzValue = parseInt(fizzValue);
+  buzzValue = parseInt(buzzValue);
+
+  //Check to see if inputted values are integers
+  if(Number.isInteger(fizzValue) && Number.isInteger(buzzValue)){
+
+    //Call fizzBuzz
+    let fbArray = fizzBuzz(fizzValue, buzzValue);
+
+    //Display result
+    displayData(fbArray);
+
+  } else {
+    alert("Please use integers")
+  }
+
+}
+
+//Generate values from start to end
+//Logics
+function fizzBuzz(fizzValue, buzzValue){
+
+  let returnArray = [];
+
+  //Create array with numbers from 1-100
+  for (let i = 1; i <= 100; i++) {
+
+    //Check for fizz and buzz Values
+    if(i % fizzValue == 0 && i % buzzValue == 0) {
+
+      returnArray.push('FizzBuzz');
+
+    } else if (i % fizzValue == 0){
+      returnArray.push('Fizz')
+
+    } else if(i % buzzValue == 0){
+      returnArray.push('Buzz')
+
+    } else {
+      returnArray.push(i);
+
+    }    
+  }
+
+  return returnArray;
+
+}
+
+//Display results
+//View
+function displayData(fbArray){
+
+  //Get table body
+  let tableBody = document.getElementById("results");
+
+  //Get the template itself
+  let templateRow = document.getElementById("fbTemplate");
+
+  //Clear table
+  tableBody.innerHTML = "";
+
+  for (let index = 0; index < fbArray.length; index += 5) {
+    let tableRow = document.importNode(templateRow.content, true);
+
+    //Grab tb of the table and put into array
+    let rowCols = tableRow.querySelectorAll("td");
+
+    rowCols[0].classList.add(fbArray[index]);
+    rowCols[0].textContent = fbArray[index];
+
+    rowCols[1].classList.add(fbArray[index +1]);
+    rowCols[1].textContent = fbArray[index +1];
+
+    rowCols[2].classList.add(fbArray[index +2]);
+    rowCols[2].textContent = fbArray[index +2];
+
+    rowCols[3].classList.add(fbArray[index +3]);
+    rowCols[3].textContent = fbArray[index +3];
+
+    rowCols[4].classList.add(fbArray[index +4]);
+    rowCols[4].textContent = fbArray[index +4];
+
+    tableBody.appendChild(tableRow);
+    
+  }
+
+  //Add all rows to the table
+
+
+
 }
